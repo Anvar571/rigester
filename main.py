2fdb1d3b10debc377b393a00eb611023a97e4fbd
Anvar571 <abdurashidovanvar52@gmail.com>
2022-08-22 12:05:26 +0500

    created methods

---
+++ main.py
@@ -0,0 +1,23 @@
+class Rigister:
+    def __init__(self, name, lName, age, job, parol):
+        self.name = name
+        self.age = age
+        self.lName = lName
+        self.job = job
+        self.parol = parol
+    def rister(self):
+        pass
+    def showData(self):
+        pass
+    def signIN(self):
+        pass
+    def getById(self):
+        pass
+    def checkLoginAndCHeckParol(self):
+        pass
+    def changePassword(self):
+        pass
+    def deleteLogin(self):
+        pass
+    def updeteLoginANDParol(self):
+        pass
