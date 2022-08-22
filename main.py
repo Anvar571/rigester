4e6e8701ac61d3eb8b1d76e49129c1eb7c04aed9
Anvar571 <abdurashidovanvar52@gmail.com>
2022-08-22 15:52:02 +0500

    rigistered ten person

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import psycopg2
 from faker import Faker
 fake = Faker()
+import random as r
 
 conn = psycopg2.connect(
     host="localhost",
@@ -22,7 +23,11 @@ class Rigister:
     def writeDatabase(self):
         query = f'''
             insert into datas(Ulogin, Fname,lname, parol, age, job)values
+            ('{self.Ulogin}', '{self.Fname}', '{self.lName}', '{self.parol}', '{self.age}', '{self.job}')
         '''
+        cursor.execute(query)
+        conn.commit()
+
     def rister(self):
         pass
     def showData(self):
@@ -39,5 +44,16 @@ class Rigister:
         pass
     def updeteLoginANDParol(self):
         pass
+    def generetePassword():
+        login = "adqvqjcQWCEQCLKLh23874ddcasSDFSDtv232$Q$3487238v2y39rbjd7f826!&#@^!T#*T!@(798yv2"
+        s = ""
+        for i in range(8):
+            s += login[r.randint(i, len(login)-1)]
+        return s
+lst = []
+for i in range(10):
+    user = Rigister(fake.name().split()[1], fake.name().split()[0], fake.name().split()[1], fake.password(), r.randint(10, 80), fake.job())
+    lst.append(user)
 
-print("HEllo")
+for i in lst:
+    i.writeDatabase()
